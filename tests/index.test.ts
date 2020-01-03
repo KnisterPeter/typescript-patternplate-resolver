109140e2eaeb6804f6b765b97281fd7973bce04b
Markus Wolf <markus.wolf@new-work.se>
2020-01-03 09:55:42 +0100

    fix: ava types

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,3 +1,6 @@
+// xxx: workaround https://github.com/avajs/ava/issues/2151
+declare global { interface SymbolConstructor { readonly observable: symbol; } }
+
 // tslint:disable-next-line:no-implicit-dependencies
 import test from 'ava';
 
